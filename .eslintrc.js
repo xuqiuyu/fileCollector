27dc31c11fc1a67671c4a32bc900de8f3ec64c19
xuqiuyu <xuqiuyu@zhongan.com>
2018-03-01 12:14:10 +0800

    npm publish

---
+++ .eslintrc.js
@@ -0,0 +1,7 @@
+module.exports = {
+    "extends": "funsee",
+    "rules": {
+      "no-debugger": 0
+    }
+  };
+  
--- index.js
+++ index.js
@@ -1,45 +1,40 @@
 #!/usr/bin/env node
-// require('babel-register')({
-//     presets: ['env','stage-0'],
-//     plugins: ['add-module-exports']
-//   });
-
 const fs = require('fs');
 const searchFile = require('./tools/searchFile');
 
-//get script command params
+// get script command params
 // const params = process.argv.reverse();
 
 console.log('collecting router files ....');
 
-const pathArray = searchFile('./shared/pages','router.js');
+const filePathArray = searchFile('./shared/pages', 'router.js');
 
-//business code
+// business code
 const generateRouter = (pathArray) => {
-    const routes = [
+  const routes = [
+    {
+      component: "require('./pages/App')",
+      routes: [
         {
-          component: "require('./pages/App')",
-          routes: [
-            {
-                path: "'/'",
-                exact: true,
-                component: "require('./components/welcome')"
-            }
-          ]
+          path: "'/'",
+          exact: true,
+          component: "require('./components/welcome')"
         }
-    ];
-    if(pathArray && Array.isArray(pathArray)){
-        pathArray.forEach(item => {
-            const routerPath = item.replace('/shared','');
-            const routerPrefix = item.split('/').length > 4 ? item.replace('./shared/pages','') : '';
-            routes[0].routes.push(
-                "...require('"+ routerPath + "/router.js').map(R => {R.path = '"+ routerPrefix + "' + R.path || ''; R.exact = R.exact || true; return R;})"
-            );
-        });
+      ]
     }
-    //写文件
-    const result = `module.exports = ${JSON.stringify(routes,null,2)}`.replace(/"/g,'');
-    fs.writeFileSync('./shared/router.js',result);
-}
+  ];
+  if (pathArray && Array.isArray(pathArray)) {
+    pathArray.forEach((item) => {
+      const routerPath = item.replace('/shared', '');
+      const routerPrefix = item.split('/').length > 4 ? item.replace('./shared/pages', '') : '';
+      routes[0].routes.push(
+        `...require('${routerPath}/router.js').map(R => {R.path = '${routerPrefix}' + R.path || ''; R.exact = R.exact || true; R.moduleName = R.component.moduleName || ''; return R;})`
+      );
+    });
+  }
+  // 写文件
+  const result = `module.exports = ${JSON.stringify(routes, null, 2)}`.replace(/"/g, '');
+  fs.writeFileSync('./shared/router.js', result);
+};
 
-generateRouter(pathArray);
+generateRouter(filePathArray);
